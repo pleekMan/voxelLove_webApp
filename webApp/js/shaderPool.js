7f8d2d6519433af9dd33e74ee59872c273fd6cdf
PleekMan <agusanzorena@gmail.com>
2019-03-12 00:40:58 -0400

    Working on Feature: load examples from shaderPool

---
+++ webApp/js/shaderPool.js
@@ -0,0 +1,28 @@
+var shaderPool = [
+	{
+		name: "shader1",
+		data: [
+			"const float PI = 3.14159265358; // SHADER CARGADO\n",
+			"const float TWO_PI = 6.28318530718;",
+			"uniform vec3 controls;",
+			"uniform float time; // IN SECONDS",
+			"varying vec4 vertexColor;",
+			"",
+			"void main() {",
+			"",
+			"float wavyX = position.x * tan(position.y * TWO_PI + (time * controls.x ) ); // DRAG THE RED SLIDER",
+			"float offsetY = position.y + controls.y; // DRAG THE GREEN SLIDER",
+			"vec4 finalPos = vec4(wavyX, offsetY, position.z, 1.0);",
+			"",
+			"gl_Position = projectionMatrix * modelViewMatrix * finalPos;",
+			"vertexColor = vec4(color,1.0);",
+			"",
+			"}"
+		]
+
+	},
+	{
+		name: "default2",
+		data: " "
+	}
+]
