472ebd80951a819f4ca094e0b3b1dce2befbc998
PleekMan <agusanzorena@gmail.com>
2019-03-12 19:46:35 -0400

    Works: loadind shaders from dropdown -> shaderPool -> compiling

--- webApp/js/shaderPool.js
+++ webApp/js/shaderPool.js
@@ -2,7 +2,8 @@ var shaderPool = [
 	{
 		name: "shader1",
 		data: [
-			"const float PI = 3.14159265358; // SHADER CARGADO\n",
+			"// SHADER 1",
+			"const float PI = 3.14159265358; // SHADER CARGADO",
 			"const float TWO_PI = 6.28318530718;",
 			"uniform vec3 controls;",
 			"uniform float time; // IN SECONDS",
@@ -22,7 +23,25 @@ var shaderPool = [
 
 	},
 	{
-		name: "default2",
-		data: " "
+		name: "shader2",
+		data:[
+			"// SHADER 2",
+			"const float PI = 3.14159265358; // SHADER CARGADO",
+			"const float TWO_PI = 6.28318530718;",
+			"uniform vec3 controls;",
+			"uniform float time; // IN SECONDS",
+			"varying vec4 vertexColor;",
+			"",
+			"void main() {",
+			"",
+			"float wavyX = position.x * tan(position.y * TWO_PI + (time * controls.x ) ); // DRAG THE RED SLIDER",
+			"float offsetY = position.y + controls.y; // DRAG THE GREEN SLIDER",
+			"vec4 finalPos = vec4(wavyX, offsetY, position.z, 1.0);",
+			"",
+			"gl_Position = projectionMatrix * modelViewMatrix * finalPos;",
+			"vertexColor = vec4(color,1.0);",
+			"",
+			"}"
+		]
 	}
 ]
